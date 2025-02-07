bbf274949eadf3e52dced4e48007c49584e7a23d
Dan Stroot <dan.stroot@gmail.com>
2025-02-07 09:03:36 -0800

    updated dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Toaster } from "sonner";
 
 import { ThemeProvider } from "next-themes";
-
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,25 +14,25 @@ export const viewport = {
   maximumScale: 1, // Disable auto-zoom on mobile Safari
 };
 
-const LIGHT_THEME_COLOR = "hsl(0 0% 100%)";
-const DARK_THEME_COLOR = "hsl(240deg 10% 3.92%)";
-const THEME_COLOR_SCRIPT = `\
-(function() {
-  var html = document.documentElement;
-  var meta = document.querySelector('meta[name="theme-color"]');
-  if (!meta) {
-    meta = document.createElement('meta');
-    meta.setAttribute('name', 'theme-color');
-    document.head.appendChild(meta);
-  }
-  function updateThemeColor() {
-    var isDark = html.classList.contains('dark');
-    meta.setAttribute('content', isDark ? '${DARK_THEME_COLOR}' : '${LIGHT_THEME_COLOR}');
-  }
-  var observer = new MutationObserver(updateThemeColor);
-  observer.observe(html, { attributes: true, attributeFilter: ['class'] });
-  updateThemeColor();
-})();`;
+// const LIGHT_THEME_COLOR = "hsl(0 0% 100%)";
+// const DARK_THEME_COLOR = "hsl(240deg 10% 3.92%)";
+// const THEME_COLOR_SCRIPT = `\
+// (function() {
+//   var html = document.documentElement;
+//   var meta = document.querySelector('meta[name="theme-color"]');
+//   if (!meta) {
+//     meta = document.createElement('meta');
+//     meta.setAttribute('name', 'theme-color');
+//     document.head.appendChild(meta);
+//   }
+//   function updateThemeColor() {
+//     var isDark = html.classList.contains('dark');
+//     meta.setAttribute('content', isDark ? '${DARK_THEME_COLOR}' : '${LIGHT_THEME_COLOR}');
+//   }
+//   var observer = new MutationObserver(updateThemeColor);
+//   observer.observe(html, { attributes: true, attributeFilter: ['class'] });
+//   updateThemeColor();
+// })();`;
 
 export default async function RootLayout({
   children,
@@ -49,13 +48,13 @@ export default async function RootLayout({
       // https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
       suppressHydrationWarning
     >
-      <head>
+      {/* <head>
         <script
           dangerouslySetInnerHTML={{
             __html: THEME_COLOR_SCRIPT,
           }}
         />
-      </head>
+      </head> */}
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
